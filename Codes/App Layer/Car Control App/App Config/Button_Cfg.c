61c9448257c7330868c93fdf9d8a443d8950b3a2
AhmedNabil <ahmednabil.gad18@gmail.com>
2021-07-30 20:48:55 +0200

    Car Control System Application Added

---
+++ Codes/App Layer/Car Control App/App Config/Button_Cfg.c
@@ -0,0 +1,16 @@
+/*
+ * Button_Cfg.c
+ *
+ * Created: 7/16/2021 9:08:21 PM
+ *  Author: Ahmed Nabil
+ */ 
+#include "Button.h"
+
+/* Array holds the used Buttons' DIO Channels */
+uint8_t gau8_buttonsUsed[BUTTONS_USED_NUM] =
+{
+	DIO_BUTTON1_PD0_CHANNEL_ID, 
+	DIO_BUTTON2_PD1_CHANNEL_ID, 
+	DIO_BUTTON3_PD2_CHANNEL_ID, 
+	DIO_BUTTON4_PD3_CHANNEL_ID
+};
---
+++ Codes/App Layer/Car Control App/App Config/Button_Cfg.h
@@ -0,0 +1,23 @@
+/*
+ * Button_Cfg.h
+ *
+ * Created: 7/15/2021 6:36:31 PM
+ *  Author: Ahmed Nabil
+ */ 
+
+
+#ifndef BUTTON_CFG_H_
+#define BUTTON_CFG_H_
+
+#include "..\..\MCAL\Dio Module\Dio.h"
+
+/* Number of Buttons Used in the System */
+#define BUTTONS_USED_NUM		4U
+
+/* ID for each Button used */
+#define BUTTON_1_M		0		
+#define BUTTON_2_L		1		
+#define BUTTON_3_R		2		
+#define BUTTON_4_G		3		
+	
+#endif /* BUTTON_CFG_H_ */
